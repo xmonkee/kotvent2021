2fe7cc32686bde58b170225e396c457edfaadc12
Mayank Mandava <mayank.mandava@afterpay.com>
2021-12-12 08:09:29 -0800

    Day11

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,10 +11,10 @@ fun main(args: Array<String>) {
 //    day6()
 //    day7()
 //    day8()
-    day9()
+//    day9()
+//    day10()
+    day11()
 }
-
-
 
 val day1 = sequence<Int> {
     val inp = Utils.getNumInput(1)
@@ -297,6 +297,157 @@ fun day9() {
     print(basins.sortedDescending().take(3).fold(1, Int::times))
     return
 
+}
+
+fun day10() {
+    val inp = Utils.getListInput(10)
+//    val inp = """[({(<(())[]>[[{[]{<()<>>
+//[(()[<>])]({[<{<<[]>>(
+//{([(<{}[<>[]}>{[]{[(<()>
+//(((({<>}<{<{<>}{[]{[]{}
+//[[<[([]))<([[{}[[()]]]
+//[{[{({}]{}}([{[{{{}}([]
+//{<[[]]>}<{[{[{[]{()[[[]
+//[<(<(<(<{}))><([]([]()
+//<{([([[(<>()){}]>(<<{{
+//<{([{{}}[<[[[<>{}]]]>[]]""".split("\n")
+
+    val matches = hashMapOf<Char, Char>(
+        '(' to ')',
+        '[' to ']',
+        '{' to '}',
+        '<' to '>'
+    )
+
+    fun getFirstBadChar(s: String): Char? {
+        val stack = Stack<Char>();
+        for (c in s) {
+            if ("{[<(".toList().contains(c)) {
+                stack.push(c)
+            } else {
+                val t = stack.pop()
+                if (matches[t] != c) {
+                    return c
+                }
+            }
+        }
+        return null
+    }
+    val badChars = inp.map{getFirstBadChar(it)}.filterNotNull();
+    val score = badChars.map{c ->
+        when (c) {
+            ')' -> 3
+            ']' -> 57
+            '}' -> 1197
+            '>' -> 25137
+            else -> 0
+        }
+    }.sum()
+    print(score)
+
+
+    // part 2
+
+    fun complete(s: String): BigInteger {
+        val stack = Stack<Char>();
+        for (c in s) {
+            if ("{[<(".toList().contains(c)) {
+                stack.push(c)
+            } else {
+                val t = stack.pop()
+                if (matches[t] != c) {
+                    return BigInteger.ZERO
+                }
+            }
+        }
+        var score: BigInteger = BigInteger.ZERO
+        while (stack.size > 0) {
+           val t = stack.pop()
+           score *= BigInteger.valueOf(5)
+            score += when(t) {
+                '(' -> BigInteger.ONE
+                '[' -> BigInteger.TWO
+                '{' -> BigInteger.valueOf(3)
+                '<' -> BigInteger.valueOf(4)
+                else -> BigInteger.ZERO
+            }
+        }
+        println(s)
+        println(score)
+        return score
+    }
+    val scores = inp.map{complete(it)}.filter { it > BigInteger.ZERO }.sorted()
+    println(scores[scores.size/2])
+
+}
+
+fun day11() {
+    val inp = Utils.getListInput(11)
+//    val inp = """5483143223
+//2745854711
+//5264556173
+//6141336146
+//6357385478
+//4167524645
+//2176841721
+//6882881134
+//4846848554
+//5283751526""".split("\n")
+   val inpp = inp.map{row -> row.map{c -> c.digitToInt()}.toMutableList()}
+
+    val H = inpp.size
+    val W = inpp[0].size
+
+    var flashes = 0
+
+    fun flash(i: Int, j: Int): List<Pair<Int, Int>> {
+//        println(inpp.map{it.joinToString(" "){it.toString().padStart(2)} }.joinToString ("\n") + "\n")
+        flashes++;
+        val toFlash = mutableListOf<Pair<Int, Int>>()
+        for(k in i-1..i+1)
+            for (l in j-1..j+1)
+                if (k in 0 until H && l in 0 until W)
+                    if (k != i || l != j)
+                        if(++inpp[k][l] == 10)
+                            toFlash.add(Pair(k, l))
+        return toFlash
+    }
+
+    fun step() {
+        val toFlash = Stack<Pair<Int, Int>>()
+
+        for (i in 0 until H) {
+            for (j in 0 until W) {
+                inpp[i][j]++
+                if(inpp[i][j] == 10) {
+                    toFlash.add(Pair(i, j))
+                }
+            }
+        }
+
+        while(toFlash.isNotEmpty()) {
+            val (i, j) = toFlash.pop()
+            toFlash.addAll(flash(i, j))
+        }
+
+        inpp.forEach{it.replaceAll { if (it > 9) 0 else it }}
+//        println(inpp.map{it.joinToString(" "){it.toString().padStart(2)} }.joinToString ("\n") + "\n")
+    }
+//    println(inpp.map{it.joinToString(" "){it.toString().padStart(2)} }.joinToString ("\n") + "\n")
+    (1..100).forEach{
+        step()
+    }
+    println(inpp.map{it.joinToString(" "){it.toString().padStart(2)} }.joinToString ("\n") + "\n")
+    println(flashes)
+
+    // part 2
+
+    var steps = 100
+    while (!inpp.all {row -> row.all{it == 0}}) {
+        step()
+        steps++
+    }
+    println(steps)
 }
 
 fun <R> permutations(l: List<R>): List<List<R>> {
